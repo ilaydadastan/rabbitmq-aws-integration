15eb0f053b0b4648552a63f8c9842d1c25fafeaa
ilaydadastan <ilaydastan2@gmail.com>
2024-04-20 12:01:44 +0300

    Initial commit

---
+++ src/main/java/com/example/rabbitmqawsintegration/RabbitmqAwsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqawsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqAwsIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqAwsIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rabbitmqawsintegration/RabbitmqAwsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqawsintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqAwsIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
